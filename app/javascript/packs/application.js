779abd3ccd548337c137214e0482431de8236885
Harshita Verma <vermaharshita9741@gmail.com>
2020-08-27 00:49:15 +0530

    icon done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
+require("semantic-ui-sass") 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
+//= require jquery
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//=  require semantic-ui
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
